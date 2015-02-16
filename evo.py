2db9acbabe88e782499204320c8bbf902b569ab2
marriola <azathoth500@gmail.com>
2015-02-15 21:39:52 -0500

    Add rats to maze

--- evo.py
+++ evo.py
@@ -4,6 +4,7 @@
 
 import curses
 from curses import wrapper
+import random
 
 ###############################################################################
 # LOCAL IMPORTS
@@ -22,20 +23,45 @@ MAZE_HEIGHT = 25
 STABLE_WIDTH = 80
 STABLE_HEIGHT = 25
 
+RAT_NAMES = ["Bonnie", "Clyde", "Zeke", "Biff", "Randy", "Jax", "Walter", "Gale", "Nova", "Boof", "Sam", "Huell"]
+
+
+###############################################################################
+# GAME STATE
+###############################################################################
+
+stable = []
+maze = []
+
+
+###############################################################################
+
+def occupied(row, col):
+    return maze[row][col] != ' '
+
+###############################################################################
+
+def load_maze():
+    maze_file = open("maze", "r")
+    for line in maze_file:
+        maze.append(line[1:-1])
+    maze_file.close()
 
 ###############################################################################
 
 def draw_maze(mazewin):
-    mazewin.addstr(0, 0, "+" + "-" * MAZE_WIDTH + "+");
+    mazewin.addstr(0, 0, "+" + "-" * (MAZE_WIDTH - 1) + "+");
     for n in range(0, MAZE_HEIGHT):
-        mazewin.addstr(n + 1, 0, "|" + " " * MAZE_WIDTH + "|")
-    mazewin.addstr(MAZE_HEIGHT, 0, "+" + "-" * MAZE_WIDTH + "+")
+        mazewin.addstr(n + 1, 0, "|" + maze[n] + "|")
+    mazewin.addstr(MAZE_HEIGHT + 1, 0, "+" + "-" * (MAZE_WIDTH - 1) + "+")
 
+    for dude in stable:
+        mazewin.addch(dude.row + 1, dude.col + 1, dude.name[0], curses.color_pair(dude.color))
 
 ###############################################################################
 
 def draw_stable(stablewin, stable):
-    categories = [("#", 2), ("NAME", 16), ("HEALTH", 7), ("MAX HEALTH", 11)]
+    categories = [("#", 2), ("NAME", 16), ("HEALTH", 7), ("MAX HEALTH", 11), ("SNIFF", 6)]
     category_columns = [0]
 
     stablewin.addstr(0, 0, " " * STABLE_WIDTH, curses.A_REVERSE)
@@ -45,7 +71,7 @@ def draw_stable(stablewin, stable):
         category_columns.append(category_columns[n] + category[1] + 1)
 
     for (n, dude) in enumerate(stable):
-        stablewin.addstr(n + 1, category_columns[0], str(n + 1))
+        stablewin.addstr(n + 1, category_columns[0], str(n + 1), curses.color_pair(dude.color))
         stablewin.addstr(n + 1, category_columns[1], dude.name)
         stablewin.addstr(n + 1, category_columns[2], str(dude.health))
         stablewin.addstr(n + 1, category_columns[3], str(dude.max_health))
@@ -55,12 +81,33 @@ def draw_stable(stablewin, stable):
 
 ###############################################################################
 
+def game_setup():
+    for n in range(0, 16):
+        curses.init_pair(n + 1,
+                         7 if (n + 1) % 8 == 0 else 0,
+                         (n + 1) % 8)
+
+    load_maze()
+
+    # initialize stable
+    for (color, name) in enumerate(RAT_NAMES):
+        row = random.randint(0, MAZE_HEIGHT)
+        col = random.randint(0, MAZE_WIDTH)
+        stable.append(rat(color, row, col, name, rat.DEFAULT_MAX_HEALTH))
+        # don't really know why this doesn't work
+        # while occupied(row, col):
+        #     stable[len(stable) - 1].row = random.randint(0, MAZE_HEIGHT)
+        #     stable[len(stable) - 1].col = random.randint(0, MAZE_WIDTH)
+
+###############################################################################
+
 def main(stdscr):
-    stable = [rat("clyde", rat.DEFAULT_MAX_HEALTH)]
+    game_setup()
+
     stablewin = curses.newwin(STABLE_HEIGHT + 1, STABLE_WIDTH, 0, MAZE_WIDTH + 3)
     draw_stable(stablewin, stable)
 
-    mazewin = curses.newwin(MAZE_HEIGHT + 2, MAZE_WIDTH + 2, 0, 0)
+    mazewin = curses.newwin(MAZE_HEIGHT + 3, MAZE_WIDTH + 2, 0, 0)
     draw_maze(mazewin)
 
     mazewin.refresh()
--- rat.py
+++ rat.py
@@ -2,12 +2,18 @@ class rat:
     DEFAULT_MAX_HEALTH = 100
     DEFAULT_SNIFF_DISTANCE = 5
 
+    color = 1
     name = ""
     health = DEFAULT_MAX_HEALTH
     max_health = DEFAULT_MAX_HEALTH
     sniff_distance = DEFAULT_SNIFF_DISTANCE
+    row = 0
+    col = 0
 
-    def __init__(self, name, max_health):
+    def __init__(self, color, row, col, name, max_health):
+        self.color = color
         self.name = name
         self.health = max_health
         self.max_health = max_health
+        self.row = row
+        self.col = col
